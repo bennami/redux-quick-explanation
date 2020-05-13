17b5485d84e854518d8c7d51a327c6d8a10dfa66
imane <benbenimane@hotmail.com>
2020-05-13 18:51:20 +0200

    quick explanation of redux on index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,46 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore} from "redux";
+
+
+
+
+
+//action, describes what you wanna do
+const increment =()=>{
+    return{
+        type: 'INCREMENT'
+    }
+}
+
+const decrement =()=>{
+    return{
+        type: 'DECREMENT'
+    }
+}
+
+//reducer
+const  counter  =  (state=0,  action)=>{
+    switch (action.type) {
+        case  "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            break;
+    }
+}
+
+//store, your globalized state
+let store = createStore(counter)
+
+//display in console
+store.subscribe(()=> console.log(store.getState()))
+
+//dispatch
+store.dispatch(increment());
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
